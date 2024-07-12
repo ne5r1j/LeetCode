1264265d7f3736c53e5f512676883eced4933864
Neeraj <118054160+ne5r1j@users.noreply.github.com>
2024-07-12 20:42:10 +0530

    Update two-sum.py

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # for each number, we check if (target-number) is in the array or not
         for i in range(len(nums)):
             if (target-nums[i]) in nums:
+                # what if target is 6 and we have 3 --> it will return same indexes twice, so this condition
                 if i == nums.index(target-nums[i]):
                     continue
                 else:
