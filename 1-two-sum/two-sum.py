7fb522c88349af63ca824507064bb72581abfd68
Neeraj <118054160+ne5r1j@users.noreply.github.com>
2024-07-12 20:46:53 +0530

    Time: 65 ms (46.38%) | Memory: 17.8 MB (26.43%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for each number, we check if (target-number) is in the array or not
-        for i in range(len(nums)):
-            if (target-nums[i]) in nums:
-                # what if target is 6 and we have 3 --> it will return same indexes twice, so this condition
-                if i == nums.index(target-nums[i]):
-                    continue
-                else:
-                    return [i, nums.index(target-nums[i])]
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
         
